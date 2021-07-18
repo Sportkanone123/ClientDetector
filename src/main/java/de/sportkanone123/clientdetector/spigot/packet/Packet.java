388f2e2c1e32d508843398374b64328cf073d2e5
Sportkanone123 <kim-mueller@gmx.com>
2021-07-18 14:31:30 +0200

    Updated structure

--- src/main/java/de/sportkanone123/clientdetector/spigot/packet/Packet.java
+++ src/main/java/de/sportkanone123/clientdetector/spigot/packet/Packet.java
@@ -1,14 +1,25 @@
+/*
+ * This file is part of ClientDetector - https://github.com/Sportkanone123/ClientDetector
+ * Copyright (C) 2021 Sportkanone123
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.sportkanone123.clientdetector.spigot.packet;
 
 import io.github.retrooper.packetevents.packettype.PacketType;
 import io.github.retrooper.packetevents.packetwrappers.NMSPacket;
-
-
-/*
-ClientDetector uses PacketEvents (by retrooper) for packet processing,
-License and other information can be found here: https://github.com/retrooper/packetevents/blob/dev/LICENSE
-*/
-
 
 public class Packet {
     private final NMSPacket rawPacket;
