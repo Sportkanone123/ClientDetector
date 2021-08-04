064bcfa5148948eb0305c040600204db21261cad
Sportkanone123 <68001069+Sportkanone123@users.noreply.github.com>
2021-08-04 18:51:14 +0200

    Added Bedrock Detection (GeyserMC), updates bungee.yml and plugin.yml

--- src/main/java/de/sportkanone123/clientdetector/spigot/manager/GeyserManager.java
+++ src/main/java/de/sportkanone123/clientdetector/spigot/manager/GeyserManager.java
@@ -24,19 +24,6 @@ import org.bukkit.entity.Player;
 
 public class GeyserManager {
     public static boolean isBedrockPlayer(Player player){
-        if(Bukkit.getPluginManager().isPluginEnabled("floodgate-bukkit") || Bukkit.getPluginManager().isPluginEnabled("floodgate-bungee")  || Bukkit.getPluginManager().isPluginEnabled("floodgate-common")){
-            //return org.geysermc.floodgate.FloodgateAPI.isBedrockPlayer(player);
-        }
-
-        /*if(Bukkit.getPluginManager().isPluginEnabled("Geyser-Spigot") || Bukkit.getPluginManager().isPluginEnabled("Geyser-BungeeCord")  || Bukkit.getPluginManager().isPluginEnabled("Geyser-Sponge") || Bukkit.getPluginManager().isPluginEnabled("Geyser")){
-            org.geysermc.connector.network.session.GeyserSession session = org.geysermc.connector.GeyserConnector.getInstance().getPlayerByUuid(player.getUniqueId());
-
-            if(session == null)
-                return false;
-            else
-                return true;
-        }*/
-
         return PacketEvents.get().getPlayerUtils().isGeyserPlayer(player.getPlayer());
     }
 }
