0f8dafeedd62b205721e5a3c3b44242111b00ea4
Sportkanone123 <68001069+Sportkanone123@users.noreply.github.com>
2021-09-22 14:29:13 +0200

    Fixed problem with BitDefender AntiVirus (false detection fixed)

--- src/main/java/de/sportkanone123/clientdetector/spigot/discord/DiscordWebhook.java
+++ src/main/java/de/sportkanone123/clientdetector/spigot/discord/DiscordWebhook.java
@@ -40,10 +40,6 @@ import java.util.Set;
 public class DiscordWebhook {
 
     private final String url;
-    private String content;
-    private String username;
-    private String avatarUrl;
-    private boolean tts;
     private final List<EmbedObject> embeds = new ArrayList<>();
 
     /**
@@ -55,37 +51,13 @@ public class DiscordWebhook {
         this.url = url;
     }
 
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setAvatarUrl(String avatarUrl) {
-        this.avatarUrl = avatarUrl;
-    }
-
-    public void setTts(boolean tts) {
-        this.tts = tts;
-    }
-
     public void addEmbed(EmbedObject embed) {
         this.embeds.add(embed);
     }
 
     public void execute() throws IOException {
-        if (this.content == null && this.embeds.isEmpty()) {
-            throw new IllegalArgumentException("Set content or add at least one EmbedObject");
-        }
 
         JSONObject json = new JSONObject();
-
-        json.put("content", this.content);
-        json.put("username", this.username);
-        json.put("avatar_url", this.avatarUrl);
-        json.put("tts", this.tts);
 
         if (!this.embeds.isEmpty()) {
             List<JSONObject> embedObjects = new ArrayList<>();
@@ -95,7 +67,6 @@ public class DiscordWebhook {
 
                 jsonEmbed.put("title", embed.getTitle());
                 jsonEmbed.put("description", embed.getDescription());
-                jsonEmbed.put("url", embed.getUrl());
 
                 if (embed.getColor() != null) {
                     Color color = embed.getColor();
@@ -107,10 +78,7 @@ public class DiscordWebhook {
                 }
 
                 EmbedObject.Footer footer = embed.getFooter();
-                EmbedObject.Image image = embed.getImage();
                 EmbedObject.Thumbnail thumbnail = embed.getThumbnail();
-                EmbedObject.Author author = embed.getAuthor();
-                List<EmbedObject.Field> fields = embed.getFields();
 
                 if (footer != null) {
                     JSONObject jsonFooter = new JSONObject();
@@ -120,13 +88,6 @@ public class DiscordWebhook {
                     jsonEmbed.put("footer", jsonFooter);
                 }
 
-                if (image != null) {
-                    JSONObject jsonImage = new JSONObject();
-
-                    jsonImage.put("url", image.getUrl());
-                    jsonEmbed.put("image", jsonImage);
-                }
-
                 if (thumbnail != null) {
                     JSONObject jsonThumbnail = new JSONObject();
 
@@ -134,27 +95,7 @@ public class DiscordWebhook {
                     jsonEmbed.put("thumbnail", jsonThumbnail);
                 }
 
-                if (author != null) {
-                    JSONObject jsonAuthor = new JSONObject();
 
-                    jsonAuthor.put("name", author.getName());
-                    jsonAuthor.put("url", author.getUrl());
-                    jsonAuthor.put("icon_url", author.getIconUrl());
-                    jsonEmbed.put("author", jsonAuthor);
-                }
-
-                List<JSONObject> jsonFields = new ArrayList<>();
-                for (EmbedObject.Field field : fields) {
-                    JSONObject jsonField = new JSONObject();
-
-                    jsonField.put("name", field.getName());
-                    jsonField.put("value", field.getValue());
-                    jsonField.put("inline", field.isInline());
-
-                    jsonFields.add(jsonField);
-                }
-
-                jsonEmbed.put("fields", jsonFields.toArray());
                 embedObjects.add(jsonEmbed);
             }
 
@@ -164,7 +105,7 @@ public class DiscordWebhook {
         URL url = new URL(this.url);
         HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
         connection.addRequestProperty("Content-Type", "application/json");
-        connection.addRequestProperty("User-Agent", "Java-DiscordWebhook-BY-Gelox_");
+        connection.addRequestProperty("User-Agent", "ClientDetector-Webhook");
         connection.setDoOutput(true);
         connection.setRequestMethod("POST");
 
@@ -180,14 +121,10 @@ public class DiscordWebhook {
     public static class EmbedObject {
         private String title;
         private String description;
-        private String url;
         private Color color;
 
         private Footer footer;
         private Thumbnail thumbnail;
-        private Image image;
-        private Author author;
-        private final List<Field> fields = new ArrayList<>();
 
         public String getTitle() {
             return title;
@@ -195,10 +132,6 @@ public class DiscordWebhook {
 
         public String getDescription() {
             return description;
-        }
-
-        public String getUrl() {
-            return url;
         }
 
         public Color getColor() {
@@ -213,18 +146,6 @@ public class DiscordWebhook {
             return thumbnail;
         }
 
-        public Image getImage() {
-            return image;
-        }
-
-        public Author getAuthor() {
-            return author;
-        }
-
-        public List<Field> getFields() {
-            return fields;
-        }
-
         public EmbedObject setTitle(String title) {
             this.title = title;
             return this;
@@ -235,10 +156,6 @@ public class DiscordWebhook {
             return this;
         }
 
-        public EmbedObject setUrl(String url) {
-            this.url = url;
-            return this;
-        }
 
         public EmbedObject setColor(Color color) {
             this.color = color;
@@ -255,20 +172,6 @@ public class DiscordWebhook {
             return this;
         }
 
-        public EmbedObject setImage(String url) {
-            this.image = new Image(url);
-            return this;
-        }
-
-        public EmbedObject setAuthor(String name, String url, String icon) {
-            this.author = new Author(name, url, icon);
-            return this;
-        }
-
-        public EmbedObject addField(String name, String value, boolean inline) {
-            this.fields.add(new Field(name, value, inline));
-            return this;
-        }
 
         private class Footer {
             private final String text;
@@ -297,66 +200,6 @@ public class DiscordWebhook {
 
             private String getUrl() {
                 return url;
-            }
-        }
-
-        private class Image {
-            private final String url;
-
-            private Image(String url) {
-                this.url = url;
-            }
-
-            private String getUrl() {
-                return url;
-            }
-        }
-
-        private class Author {
-            private final String name;
-            private final String url;
-            private final String iconUrl;
-
-            private Author(String name, String url, String iconUrl) {
-                this.name = name;
-                this.url = url;
-                this.iconUrl = iconUrl;
-            }
-
-            private String getName() {
-                return name;
-            }
-
-            private String getUrl() {
-                return url;
-            }
-
-            private String getIconUrl() {
-                return iconUrl;
-            }
-        }
-
-        private class Field {
-            private final String name;
-            private final String value;
-            private final boolean inline;
-
-            private Field(String name, String value, boolean inline) {
-                this.name = name;
-                this.value = value;
-                this.inline = inline;
-            }
-
-            private String getName() {
-                return name;
-            }
-
-            private String getValue() {
-                return value;
-            }
-
-            private boolean isInline() {
-                return inline;
             }
         }
     }
